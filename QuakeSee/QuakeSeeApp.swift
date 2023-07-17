a87355fd3c72805d45172db52965593e75d0ce5e
Donizetti Aparecido de Souza Junior <71847472+souza10v@users.noreply.github.com>
2023-07-17 14:20:31 +1200

    Initial Commit

---
+++ QuakeSee/QuakeSeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuakeSeeApp.swift
+//  QuakeSee
+//
+//  Created by Donizetti de Souza on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct QuakeSeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
